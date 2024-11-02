c862ec2841fdf0510fb45e9529ce8541ca1e5784
Aleff Brandao Rodrigues <aleff.rodrigues@hapvida.com.br>
2024-11-02 18:23:59 -0300

    ex005 - superglobais PHP

--- exercicios/ex005/superglobais.php
+++ exercicios/ex005/superglobais.php
@@ -11,6 +11,10 @@
         <pre>
             <?php 
                 
+                setcookie("dia-da-semana","SEGUNDA", time() + 3600);
+
+                session_start();
+                $_SESSION["teste"] = "FUNCIONOU!";
 
                 echo "<h1>Superglobal GET</h1>";
                 var_dump($_GET);
@@ -20,6 +24,21 @@
 
                 echo "<h1>Superglobal REQUEST</h1>";
                 var_dump($_REQUEST);
+
+                echo "<h1>Superglobal COOKIE</h1>";
+                var_dump($_COOKIE);
+
+                echo "<h1>Superglobal SESSION</h1>";
+                var_dump($_SESSION);
+
+                echo "<h1>Superglobal ENV</h1>";
+                var_dump($_ENV);
+
+                echo "<h1>Superglobal SERVER</h1>";
+                var_dump($_SERVER);
+
+                echo "<h1>Superglobal GLOBALS</h1>";
+                var_dump($GLOBALS);
             ?>
         </pre>
    </main> 
